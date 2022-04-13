31c6bcedafbb4259397f3e6c1f77b0ee2daf2ecf
Krisztyon <kovacs.krisztian@dami.martfu.hu>
2022-04-13 19:30:55 +0200

    asdasd

---
+++ scripts.js
@@ -0,0 +1,120 @@
+$( "#helyszinekLosSantos" ).click(function() {
+    $( "#SanFierro" ).css("visibility", "hidden");
+    $( "#Videk" ).css("visibility", "hidden");
+    $( "#LosSantos" ).css("visibility", "visible");
+    $( "#LosSantos2" ).css("visibility", "visible");
+    $( "#LosSantos3" ).css("visibility", "visible");
+});
+
+$( "#helyszinekSanFierro" ).click(function() {
+    $( "#LosSantos" ).css("visibility", "hidden");
+    $( "#Videk" ).css("visibility", "hidden");
+    $( "#LosSantos2" ).css("visibility", "hidden");
+    $( "#LosSantos3" ).css("visibility", "hidden");
+    $( "#SanFierro" ).css("visibility", "visible");
+
+});
+
+$( "#helyszinekVidek" ).click(function() {
+    $( "#LosSantos" ).css("visibility", "hidden");
+    $( "#SanFierro" ).css("visibility", "hidden");
+    $( "#LosSantos2" ).css("visibility", "hidden");
+    $( "#LosSantos3" ).css("visibility", "hidden");
+    $( "#Videk" ).css("visibility", "visible");
+});
+
+$( "#helyszinekOsszes" ).click(function() {
+    $( "#LosSantos" ).css("visibility", "visible");
+    $( "#LosSantos2" ).css("visibility", "visible");
+    $( "#LosSantos3" ).css("visibility", "visible");
+    $( "#SanFierro" ).css("visibility", "visible");
+    $( "#Videk" ).css("visibility", "visible");
+});
+
+$( "#LStojas1").click(function() {
+    $( "#LStojas1" ).css("visibility", "hidden");
+});
+
+$( "#LStojas2").click(function() {
+    $( "#LStojas2" ).css("visibility", "hidden");
+});
+
+$( "#LStojas3").click(function() {
+    $( "#LStojas3" ).css("visibility", "hidden");
+});
+
+$( "#LStojas4").click(function() {
+    $( "#LStojas4" ).css("visibility", "hidden");
+});
+
+$( "#LStojas5").click(function() {
+    $( "#LStojas5" ).css("visibility", "hidden");
+});
+
+$( "#LStojas6").click(function() {
+    $( "#LStojas6" ).css("visibility", "hidden");
+});
+
+$( "#LStojas7").click(function() {
+    $( "#LStojas7" ).css("visibility", "hidden");
+});
+
+$( "#LStojas8").click(function() {
+    $( "#LStojas8" ).css("visibility", "hidden");
+});
+
+$( "#LStojas9").click(function() {
+    $( "#LStojas9" ).css("visibility", "hidden");
+});
+
+$( "#LStojas10").click(function() {
+    $( "#LStojas10" ).css("visibility", "hidden");
+});
+
+$( "#LStojas11").click(function() {
+    $( "#LStojas11" ).css("visibility", "hidden");
+});
+
+$( "#LStojas12").click(function() {
+    $( "#LStojas12" ).css("visibility", "hidden");
+});
+
+$( "#LStojas13").click(function() {
+    $( "#LStojas13" ).css("visibility", "hidden");
+});
+
+$( "#LStojas14").click(function() {
+    $( "#LStojas14" ).css("visibility", "hidden");
+});
+
+$( "#LStojas15").click(function() {
+    $( "#LStojas15" ).css("visibility", "hidden");
+});
+
+$( "#LStojas16").click(function() {
+    $( "#LStojas16" ).css("visibility", "hidden");
+});
+
+$( "#LStojas17").click(function() {
+    $( "#LStojas17" ).css("visibility", "hidden");
+});
+
+$( "#LStojas18").click(function() {
+    $( "#LStojas18" ).css("visibility", "hidden");
+});
+
+$( "#LStojas19").click(function() {
+    $( "#LStojas19" ).css("visibility", "hidden");
+});
+
+$( "#LStojas20").click(function() {
+    $( "#LStojas20" ).css("visibility", "hidden");
+});
+
+$( "#LStojas21").click(function() {
+    $( "#LStojas21" ).css("visibility", "hidden");
+});
+
+$( "#LStojas22").click(function() {
+    $( "#LStojas22" ).css("visibility", "hidden");
+});
